289be1b339c894c6266062a5677c9e14103c83d0
임성경 <5622lsk@naver.com>
2024-07-30 20:12:39 +0900

    환경세팅

--- src/pages/stadium/stadiumPage.tsx
+++ src/pages/stadium/stadiumPage.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-
-function stadiumPage() {
-  return <div></div>;
-}
-
+const stadiumPage = () => {
+  return (
+    <>
+      <h1>stadiumPage Component</h1>
+    </>
+  );
+};
 export default stadiumPage;
