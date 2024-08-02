2af1431fc30ba275da60e38cd1a8707648d2592d
임성경 <5622lsk@naver.com>
2024-08-03 00:55:22 +0900

    feat: 경기 일정 카드 컴포넌트 (#3)

---
+++ src/components/home/Category.tsx
@@ -0,0 +1,8 @@
+const Category = () => {
+  return (
+    <>
+      <h1>Category Component</h1>
+    </>
+  );
+};
+export default Category;
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import HeroCarousel from "./HeroCarousel";
 import heroData from "./dummydata.json";
 
+import Card from "../../components/home/Card";
+
 const HomePage: React.FC = () => {
   return (
+    <>
     <HeroCarousel teams={heroData.teams} />
+      <Card />
+    </>
   );
 };
 
