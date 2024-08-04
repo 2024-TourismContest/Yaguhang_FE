d31f5bfbb12f4e8e0ef8aeba0c92b17aa4417eae
aong13 <oaoa13@pukyong.ac.kr>
2024-08-05 00:21:26 +0900

    Feat: 히어로 캐러셀 완료
    - 더미데이터 변경
    - 팀명과 지역 강조
    - 캐러셀 이동 dot 추가
    - 그라디언트 추가
    - 반응형 단위 사용

--- src/pages/home/HeroCarousel.tsx
+++ src/pages/home/HeroCarousel.tsx
@@ -11,9 +11,12 @@ interface HeroData {
     heroTitle: string;
     heroText: string;
     teamLogo: string;
+    teamName: string;
+    region: string;
   }[];
 }
 
+// 캐러셀
 const HeroCarousel: React.FC<HeroData> = ({ teams }) => {
   const settings = {
     dots: true,
@@ -24,11 +27,12 @@ const HeroCarousel: React.FC<HeroData> = ({ teams }) => {
     autoplay: true,
     vertical: true,
     autoplaySpeed: 2800,
-    arrows: false, 
+    arrows: false,
+    
   };
 
   return (
-    <Slider {...settings}>
+    <StyledSlider {...settings}>
       {teams.map((team) => (
         <HeroCarouselItem
           key={team.id}
@@ -36,106 +40,231 @@ const HeroCarousel: React.FC<HeroData> = ({ teams }) => {
           heroTitle={team.heroTitle}
           heroText={team.heroText}
           teamLogo={team.teamLogo}
-          altText={ 'Team Logo' }
+          altText={"Team Logo"}
+          teamName={team.teamName}
+          region={team.region}
         />
       ))}
-    </Slider>
+    </StyledSlider>
   );
 };
 
 interface HeroSectionProps {
-  key: string;
   heroImage: string;
   heroTitle: string;
   heroText: string;
   teamLogo: string;
   altText: string;
+  teamName: string;
+  region: string;
 }
 
 const HeroCarouselItem: React.FC<HeroSectionProps> = ({
-  key,
   heroImage,
   heroTitle,
   heroText,
   teamLogo,
   altText,
+  teamName,
+  region,
 }) => {
   const navigate = useNavigate();
+
+  // 버튼
   const handleClickBtn = () => {
-    navigate(`/${key}`);
+    navigate(`/${teamName}`);
   };
+
+  const renderStyledText = (title: string) => {
+    const parts = title.split(new RegExp(`(${region}|${teamName})`, "g"));
+    return (
+      <>
+        {parts.map((part, index) => {
+          if ([region, teamName].includes(part)) {
+            return <StyledLargeText key={index}>{part}</StyledLargeText>;
+          }
+          return <StyledNormalText key={index}>{part}</StyledNormalText>;
+        })}
+      </>
+    );
+  };
+
   return (
     <HeroContainer heroImage={heroImage}>
-      <Wrapper>
-        <HeroContentWrapper>
-          <HeroTitle>{heroTitle}</HeroTitle>
-          <HeroText>{heroText}</HeroText>
-        </HeroContentWrapper>
-        <TeamLogoContainer>
-          <TeamLogo src={teamLogo} alt={altText} />
-        </TeamLogoContainer>
-      </Wrapper>
-      <Button onClick={handleClickBtn}> 보러가기 </Button>
+      <RowWrapper>
+        <div>
+          <HeroTextContainer>
+            <HeroTitle>{renderStyledText(heroTitle)}</HeroTitle>
+            <HeroText>{heroText}</HeroText>
+          </HeroTextContainer>
+          <Button onClick={handleClickBtn}>{`관광지 보러가기 >`}</Button>
+        </div>
+        <TeamLogo src={teamLogo} alt={altText} />
+      </RowWrapper>
     </HeroContainer>
   );
 };
+const StyledSlider = styled(Slider)`
+  .slick-list {
+    overflow: hidden;
+  }
+
+  .slick-dots {
+    position: absolute;
+    bottom: -3vh;
+    width: 100%;
+    display: flex !important;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .slick-dots li {
+    margin: 0 5px;
+  }
+
+  .slick-dots li button {
+    display: block;
+    width: 9px;
+    height: 9px;
+    padding: 5px;
+    cursor: pointer;
+    color: transparent;
+    border: 0;
+    outline: none;
+    background: transparent;
+  }
+
+  .slick-dots li button:before {
+    font-size: 0;
+    line-height: 0;
+    display: block;
+    width: 9px;
+    height: 9px;
+    content: '';
+    border-radius: 50%;
+    background-color: rgba(255, 255, 255, 0.5);
+  }
+
+  .slick-dots li.slick-active button:before {
+    background-color: rgba(255, 255, 255, 1);
+  }
+`;
 
 const HeroContainer = styled.div<{ heroImage: string }>`
-  height: 52.0833vh;
-  background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
-    url(${(props) => props.heroImage});
+  position: relative;
+  height: 56.8125rem;
+  background: url(${(props) => props.heroImage}) no-repeat center center;
   background-size: cover;
-  color: white;
-  padding: 200px;
+  
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: linear-gradient(
+      to bottom, 
+      rgba(0, 0, 0, 0.857) 0%, 
+      rgba(0, 0, 0, 0.5) 20%,
+      rgba(0, 0, 0, 0.3) 50%, 
+      rgba(0, 0, 0, 0.7) 80%,
+      rgb(0, 0, 0) 100%
+    );
+    z-index: 1;
+  }
+  
+  & > * {
+    position: relative;
+    z-index: 2;
+  }
 `;
 
-const Wrapper = styled.div`
+const RowWrapper = styled.div`
   display: flex;
+  flex-direction: row;
   justify-content: space-between;
-  min-width: max-content;
+  padding-left: 11vw;
+  padding-right: 17.8283vw;
+  align-items: center;
 `;
 
-const HeroContentWrapper = styled.div`
-  min-width: max-content;
+const HeroTextContainer = styled.div`
+  padding-top: 30.7071vw;
   display: flex;
   flex-direction: column;
+  gap: 0.84vh;
+  padding-bottom: 3vw;
 `;
 
 const HeroTitle = styled.h1`
-  font-size: 46.82pt;
-  font-weight: 600;
   white-space: pre-line;
+`;
+
+const StyledLargeText = styled.span`
+  color: #fff;
+  font-family: Inter;
+  font-size: 3.40744rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+
+const StyledNormalText = styled.span`
+  color: #fff;
+  font-family: Inter;
+  font-size: 2.47713rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 `;
 
 const HeroText = styled.p`
-  font-size: 1.5em;
-  margin-top: 200px;
-  line-height: 30pt;
-  white-space: pre-line;
+  color: #fff;
+  font-family: Inter;
+  font-size: 1.125rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 `;
 
-const TeamLogoContainer = styled.div`
-  max-width: 470px;
+const TeamLogo = styled.img`
+  width: 17.5rem;
   display: flex;
   align-items: center;
   justify-content: center;
   overflow: hidden;
 `;
 
-const TeamLogo = styled.img`
-  max-width: 100%;
-  max-height: 100%;
-  object-fit: contain; /* 비율 유지하며 자르지 않고 포함됨 */
-`;
 const Button = styled.button`
-  border-radius: 10px;
-  padding: 10px 30px;
-  margin-top: 100px;
-  font-size: 1.5em;
-  font-weight: 700;
-  color: #6a6a6a;
-  border: none;
+  width: fit-content;
+  padding: 0.94rem 1.74em;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.57963rem;
+  border: 1px #d9d9d9 solid;
+  background: #ca0000;
+
+  color: #fff;
+  font-family: Inter;
+  font-size: 1.3125rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
   cursor: pointer;
+`;
+
+const DotsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Dot = styled.div`
+  width: 10px;
+  height: 10px;
+  background-color: white;
+  border-radius: 50%;
 `;
 
 export default HeroCarousel;
