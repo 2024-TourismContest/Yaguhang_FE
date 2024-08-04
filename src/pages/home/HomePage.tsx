0f4e1d813789c9bff981117f4c8f39f12b3e58a3
aong13 <oaoa13@pukyong.ac.kr>
2024-08-05 01:07:46 +0900

    :lipstick: UI: 홈화면 기본 좌우 패딩 컨테이너 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,14 +1,22 @@
 import Card from "../../components/home/Card";
 import HeroCarousel from "../../components/home/HeroCarousel";
 import heroData from "../../dummy-data/dummy-hero-data.json";
+import Weather from "../../components/home/Weather"
+import styled from "styled-components";
 
 const HomePage: React.FC = () => {
   return (
     <>
     <HeroCarousel teams={heroData.teams} />
+    <HomeContainer>
       <Card />
+      <Weather />
+    </HomeContainer>
     </>
   );
 };
 
+const HomeContainer = styled.div`
+  padding: 0 16vh
+`
 export default HomePage;
