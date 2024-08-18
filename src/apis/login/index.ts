b258aceadb34a108ec5443e2464dcbada556052d
aong13 <oaoa13@pukyong.ac.kr>
2024-08-18 21:59:56 +0900

    :sparkles: Feat: zustand로 isAuthenticated상태관리

--- src/apis/login/index.ts
+++ src/apis/login/index.ts
@@ -1,14 +1,19 @@
 import { defaultApi } from "../core/index";
+import useAuthStore from "../../store/authStore";
 
 export const login = {
   login: async (email: string, password: string) => {
     try {
       const response = await defaultApi.post("/api/auth/login", { email, password });
       const { accessToken, tokenType } = response.data;
-      // 토큰을 로컬 스토리지에 저장
+
+      const setIsAuthenticated = useAuthStore.getState().setIsAuthenticated;
+      setIsAuthenticated(true);
+
       localStorage.setItem("token", accessToken);
       localStorage.setItem("tokenType", tokenType);
-      return response.data; 
+
+      return response.data;
     } catch (error) {
       console.error("Login error:", error);
       throw error;
