a348cde90621f81f9867c44e3f172e77411666d7
dani001024 <aj9473@naver.com>
2024-09-11 22:31:04 +0900

    [FEAT] : option 선택 추가

---
+++ src/components/recomment/Option.tsx
@@ -0,0 +1,46 @@
+import { styled } from "styled-components";
+
+export const Option = ({
+  selectedOption,
+  handleOptionChange,
+}: {
+  selectedOption: string;
+  handleOptionChange: (option: string) => void;
+}) => {
+  const onChangeSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    handleOptionChange(e.target.value);
+  };
+
+  return (
+    <Container>
+      <Select value={selectedOption} onChange={onChangeSelect}>
+        <SelectOption value="인기순">인기순</SelectOption>
+        <SelectOption value="최신순">최신순</SelectOption>
+      </Select>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  margin-top: 5px;
+  margin-bottom: 5vh;
+  width: 100%;
+  display: flex;
+  justify-content: end;
+  padding-right: 30%;
+  @media (max-width: 1024px) {
+    padding-right: 10%;
+  }
+`;
+const Select = styled.select`
+  width: 75px;
+  border: 1px solid white;
+  border-radius: 2em;
+  padding: 3px 5px;
+  background-color: transparent;
+  color: white;
+`;
+const SelectOption = styled.option`
+  color: black;
+  background-color: transparent;
+`;
--- src/pages/recommend/RecommendPage.tsx
+++ src/pages/recommend/RecommendPage.tsx
@@ -3,35 +3,61 @@ import styled from "styled-components";
 import { recommend } from "../../apis/recommend";
 import { Filter } from "../../components/recomment/filter";
 import { Item } from "../../components/recomment/Item";
+import { Option } from "../../components/recomment/Option";
+import { SearchInput } from "../../components/recomment/SearchInput";
 
 export const RecommendPage = () => {
   const [recommendList, setRecommendList] = useState([]); // 상태 추가
+  const [searchWord, setSearchWord] = useState<string>("");
+  4;
+  const [selectedOption, setOption] = useState<string>("인기순");
   const [selectedSpot, setSelectedSpot] = useState("전체");
-  const hadleSpotChange = (spot: string) => {
+  const handleSpotChange = (spot: string) => {
     setSelectedSpot(spot);
   };
-
+  const handleInputChange = (word: string) => {
+    setSearchWord(word);
+  };
+  const handleOptionChange = (option: string) => {
+    setOption(option);
+  };
   useEffect(() => {
-    const getRecommendList = async () => {
-      console.log("실행됨");
-      try {
-        const response = await recommend({
-          pagdIndex: 1,
-          pageSize: 10,
-          order: "인기순",
-          filter: selectedSpot,
-        });
-        setRecommendList(response.data.recommendPreviewDtos); // 데이터 저장
-      } catch (error) {}
-    };
-
     getRecommendList();
-  }, []);
+    console.log("실행");
+  }, [selectedSpot, selectedOption]);
+
+  //데이터 fetching
+  const getRecommendList = async () => {
+    console.log("실행됨");
+    try {
+      const response = await recommend({
+        pagdIndex: 1,
+        pageSize: 10,
+        order: selectedOption,
+        filter: selectedSpot,
+      });
+      setRecommendList(response.data.recommendPreviewDtos); // 데이터 저장
+    } catch (error) {}
+  };
 
   return (
     <AppContainer>
       <TopSection />
-      <Filter selectedSpot={selectedSpot} hadleSpotChange={hadleSpotChange} />
+      <Section>
+        <Filter
+          selectedSpot={selectedSpot}
+          handleSpotChange={handleSpotChange}
+        />
+        <SearchInput
+          handleInputChange={handleInputChange}
+          getRecommendList={getRecommendList}
+          searchWord={searchWord}
+        />
+      </Section>
+      <Option
+        selectedOption={selectedOption}
+        handleOptionChange={handleOptionChange}
+      />
       <ItemWrapper>
         {recommendList.map((item) => (
           <Item key={item} item={item} /> // 각 항목을 Item 컴포넌트로 전달
@@ -47,11 +73,18 @@ const AppContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 3vh;
 `;
 const ItemWrapper = styled.div`
   display: grid;
   gap: 4vh;
+`;
+const Section = styled.section`
+  margin-top: 15px;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
 `;
 const TopSection = styled.section`
   width: 100vw;
