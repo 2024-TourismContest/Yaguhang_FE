1f7b053d2a8e7d63232d06f15ec5afa1e7eacd65
dani001024 <aj9473@naver.com>
2024-09-12 00:46:27 +0900

    [FEAT] : 페이지네이션 구현

---
+++ src/components/recommend/pagenation.tsx
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Pagenation = ({
+  lastPage,
+  currentPage,
+  setCurrentPage,
+}: {
+  lastPage: number;
+  currentPage: number;
+  setCurrentPage: (page: number) => void;
+}) => {
+  const [pageList, setPageList] = useState(0);
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const value = e.currentTarget.value;
+
+    if (value === "first") {
+      setCurrentPage(0);
+      setPageList(0);
+    } else if (value === "last") {
+      setCurrentPage(lastPage);
+      setPageList(lastPage - 8);
+    } else if (value === "prev") {
+      if (currentPage > 0) {
+        if (pageList === currentPage) {
+          setPageList(pageList - 1);
+        }
+        setCurrentPage(currentPage - 1);
+      } else if (currentPage <= pageList && currentPage !== 0) {
+        setPageList(pageList - 1);
+      }
+    } else if (value === "next") {
+      if (currentPage < lastPage) {
+        if (currentPage === pageList + 8 && currentPage !== lastPage) {
+          setPageList(pageList + 1);
+        }
+        setCurrentPage(currentPage + 1);
+      }
+    } else {
+      setCurrentPage(Number(value));
+    }
+  };
+
+  useEffect(() => {
+    setCurrentPage(0);
+    setPageList(0);
+  }, [lastPage, setCurrentPage]);
+
+  return (
+    <Wrapper>
+      <Page value="first" onClick={handleClick} disabled={currentPage === 0}>
+        &lt;&lt;
+      </Page>
+      <Page value="prev" onClick={handleClick} disabled={currentPage === 0}>
+        &lt;
+      </Page>
+      {Array.from(
+        { length: Math.min(9, lastPage - pageList) }, // lastPage에 맞춰 버튼 개수를 조정
+        (_, index) => (
+          <Page
+            key={index}
+            value={pageList + index}
+            onClick={handleClick}
+            disabled={pageList + index > lastPage}
+            isSelected={pageList + index === currentPage}>
+            {pageList + index + 1}
+          </Page>
+        )
+      )}
+      <Page
+        value="next"
+        onClick={handleClick}
+        disabled={currentPage === lastPage}>
+        &gt;
+      </Page>
+      <Page
+        value="last"
+        onClick={handleClick}
+        disabled={currentPage === lastPage}>
+        &gt;&gt;
+      </Page>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  display: flex;
+  margin: 10px;
+  justify-content: center;
+`;
+
+const Page = styled.button<{ isSelected?: boolean }>`
+  width: 30px;
+  height: 30px;
+  font-size: 16px;
+  font-weight: 600;
+
+  justify-content: center;
+  align-items: center;
+  color: ${(props) => (props.isSelected ? "#000" : "#fff")};
+  background-color: ${(props) =>
+    props.isSelected ? "#A7CFEC" : "transparent"};
+  border-radius: 50%;
+  cursor: pointer;
+  user-select: none;
+  border: none;
+  margin: 0 2px;
+
+  &:disabled {
+    color: #919191;
+    cursor: not-allowed;
+  }
+`;
+
+export default Pagenation;
--- src/pages/recommend/RecommendPage.tsx
+++ src/pages/recommend/RecommendPage.tsx
@@ -4,9 +4,12 @@ import { recommend } from "../../apis/recommend";
 import { Filter } from "../../components/recommend/filter";
 import { Item } from "../../components/recommend/Item";
 import { Option } from "../../components/recommend/Option";
+import Pagenation from "../../components/recommend/pagenation";
 import { SearchInput } from "../../components/recommend/SearchInput";
 
 export const RecommendPage = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [lastPage, setLastPage] = useState(1);
   const [recommendList, setRecommendList] = useState([]); // 상태 추가
   const [searchWord, setSearchWord] = useState<string>("");
   4;
@@ -36,6 +39,7 @@ export const RecommendPage = () => {
         order: selectedOption,
         filter: selectedSpot,
       });
+      setLastPage(response.data.totalPage);
       setRecommendList(response.data.recommendPreviewDtos); // 데이터 저장
     } catch (error) {}
   };
@@ -63,12 +67,17 @@ export const RecommendPage = () => {
           <Item key={item} item={item} /> // 각 항목을 Item 컴포넌트로 전달
         ))}
       </ItemWrapper>
+      <Pagenation
+        lastPage={lastPage}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
     </AppContainer>
   );
 };
 
 const AppContainer = styled.div`
-  width: 100vw;
+  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -81,13 +90,13 @@ const ItemWrapper = styled.div`
 const Section = styled.section`
   margin-top: 15px;
   display: flex;
-  width: 100%;
+  width: 100vw;
   justify-content: center;
   align-items: center;
   gap: 15px;
 `;
 const TopSection = styled.section`
-  width: 100vw;
+  width: 100%;
   height: 30vh;
   background-color: #a7cfec;
   @media (max-width: 1024px) {
